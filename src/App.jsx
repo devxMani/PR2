bd27c4ab7840121ff828142ad676442e968e362a
MANI <chandramanigupta2003@gmail.com>
2024-08-13 00:16:41 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
-import Navigation  from "./components/Navigation";
+import Navigation from "./components/Navigation";
+import ContactHeader from "./components/Navigation/ContactHeader/ContactHeader";
 
 const App = () => {
   return (
     <div>
       <Navigation />
+      <ContactHeader />
     </div>
-  )
-}
+  );
+};
 
 export default App;
-
